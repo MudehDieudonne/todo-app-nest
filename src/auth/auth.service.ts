57ab6eef6759b1ef9f0652e2da612562a8cfe3ab
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-06-18 11:55:38 +0100

    auth and validation with dtos, nestjs pipes, nest js Global pipes Hashing with argon and sign up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { msg: `I am sign up` };
+  async signup(dto: AuthDto) {
+    // Gnerate password hash
+    const hash = await argon2.hash(dto.password);
+    // Save new User on db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+      // Return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already exist');
+        }
+      }
+      throw error;
+    }
   }
 
   sigin() {
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,15 @@
-export interface AuthDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsEmail()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsNotEmpty()
   email: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsString()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsNotEmpty()
   password: string;
 }
