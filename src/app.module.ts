e0c5c011c7793f60de40b0c83c92bb41eaaee464
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-06-18 01:57:44 +0100

    prisma modules and starting with dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { TodolistModule } from './todolist/todolist.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, TodolistModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    TodolistModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    console.log({ dto });
+
     return this.authService.signup();
   }
 
